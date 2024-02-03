293dd68f39fa0444a93123849f109046e38203b7
wkazubski <wojciech.kazubski@pw.edu.pl>
2024-02-03 16:29:54 +0100

    Fix indentation

--- lib/gps_codegen_c_impl.cc
+++ lib/gps_codegen_c_impl.cc
@@ -43,18 +43,18 @@ namespace gr {
     gps_codegen_c_impl::gps_codegen_c_impl(float samplerate, int code, int datamode)
       : gr::sync_block("gps_codegen_c",
               gr::io_signature::make(0, 0, 0),
-              gr::io_signature::make(1,1, sizeof(gr_complex)))
+              gr::io_signature::make(1, 1, sizeof(gr_complex)))
     {
-        set_code(code);
+      set_code(code);
 
-        set_sample_rate(samplerate);
-        code_phase = 0;
+      set_sample_rate(samplerate);
+      code_phase = 0;
 
-        datagen_mode = datamode;
-        data_counter = 0;
+      datagen_mode = datamode;
+      data_counter = 0;
 
-        for(int i = 0; i < 22 ; ++i)
-            random_data[i] = 1;
+      for(int i = 0; i < 22 ; ++i)
+        random_data[i] = 1;
     }
 
     /*
@@ -66,246 +66,245 @@ namespace gr {
 
     void gps_codegen_c_impl::set_code(int c)
     {
-        int g1_tap0 = 0;
-        int g1_tap1 = 0;
+      int g1_tap0 = 0;
+      int g1_tap1 = 0;
 
-        code_selection = c;
+      code_selection = c;
 
-        // code generator and tap coefficients from 
-        // Matjaz Vidmar
-        // http://lea.hamradio.si/~s53mv/navsats/theory.html
+      // code generator and tap coefficients from 
+      // Matjaz Vidmar
+      // http://lea.hamradio.si/~s53mv/navsats/theory.html
 
-        switch(c) 
+      switch(c) 
+      {
+        case 1:
+          g1_tap0=1;
+          g1_tap1=5;
+          break;
+
+        case 2:
+          g1_tap0=2;
+          g1_tap1=6;
+          break;
+
+        case 3:
+          g1_tap0=3;
+          g1_tap1=7;
+          break;
+
+        case 4:
+          g1_tap0=4;
+          g1_tap1=8;
+          break;
+
+        case 5:
+          g1_tap0=0;
+          g1_tap1=8;
+          break;
+
+        case 6:
+          g1_tap0=1;
+          g1_tap1=5;
+          break;
+
+        case 7:
+          g1_tap0=0;
+          g1_tap1=7;
+          break;
+
+        case 8:
+          g1_tap0=1;
+          g1_tap1=8;
+          break;
+
+        case 9:
+          g1_tap0=2;
+          g1_tap1=9;
+          break;
+
+        case 10:
+          g1_tap0=1;
+          g1_tap1=2;
+          break;
+
+        case 11:
+          g1_tap0=2;
+          g1_tap1=3;
+          break;
+
+        case 12:
+          g1_tap0=4;
+          g1_tap1=5;
+          break;
+
+        case 13:
+          g1_tap0=5;
+          g1_tap1=6;
+          break;
+
+        case 14:
+          g1_tap0=6;
+          g1_tap1=7;
+          break;
+
+        case 15:
+          g1_tap0=7;
+          g1_tap1=8;
+          break;
+
+        case 16:
+          g1_tap0=8;
+          g1_tap1=9;
+          break;
+
+        case 17:
+          g1_tap0=0;
+          g1_tap1=3;
+          break;
+
+        case 18:
+          g1_tap0=1;
+          g1_tap1=4;
+          break;
+
+        case 19:
+          g1_tap0=2;
+          g1_tap1=5;
+          break;
+
+        case 20:
+          g1_tap0=3;
+          g1_tap1=6;
+          break;
+
+        case 21:
+          g1_tap0=4;
+          g1_tap1=7;
+          break;
+
+        case 22:
+          g1_tap0=5;
+          g1_tap1=8;
+          break;
+
+        case 23:
+          g1_tap0=0;
+          g1_tap1=2;
+          break;
+
+        case 24:
+          g1_tap0=3;
+          g1_tap1=5;
+          break;
+
+        case 25:
+          g1_tap0=4;
+          g1_tap1=6;
+          break;
+
+        case 26:
+          g1_tap0=5;
+          g1_tap1=7;
+          break;
+
+        case 27:
+          g1_tap0=6;
+          g1_tap1=8;
+          break;
+
+        case 28:
+          g1_tap0=7;
+          g1_tap1=9;
+          break;
+
+        case 29:
+          g1_tap0=0;
+          g1_tap1=5;
+          break;
+
+        case 30:
+          g1_tap0=1;
+          g1_tap1=6;
+          break;
+
+        case 31:
+          g1_tap0=2;
+          g1_tap1=7;
+          break;
+
+        case 32:
+          g1_tap0=3;
+          g1_tap1=8;
+          break;
+
+        default: // default
+          code_selection=1;
+          g1_tap0=1;
+          g1_tap1=5;
+      }
+
+      // calculate our code LUT
+
+      char g0[10] = {1,1,1,1,1,1,1,1,1,1};
+      char g1[10] = {1,1,1,1,1,1,1,1,1,1};
+      char g1_out = 0;
+
+      int i = 0, j = 0;
+      for(i = 0 ; i < 1023 ; ++i)
+      {
+        g1_out = g1[g1_tap0] ^ g1[g1_tap1];
+
+        code_LUT[i] = (gr_complex) (2.0 * ((g1_out ^ g0[9]) - 0.5));
+
+        char g0_state0_next = g0[2] ^ g0[9];
+        char g1_state0_next = g1[1] ^ g1[2] ^ g1[5]
+                             ^ g1[7] ^ g1[8] ^ g1[9];
+
+        for(j = 9; j >= 0; --j)
         {
-            case 1:
-            g1_tap0=1;
-            g1_tap1=5;
-            break;
-
-            case 2:
-            g1_tap0=2;
-            g1_tap1=6;
-            break;
-
-            case 3:
-            g1_tap0=3;
-            g1_tap1=7;
-            break;
-
-            case 4:
-            g1_tap0=4;
-            g1_tap1=8;
-            break;
-
-            case 5:
-            g1_tap0=0;
-            g1_tap1=8;
-            break;
-
-            case 6:
-            g1_tap0=1;
-            g1_tap1=5;
-            break;
-
-            case 7:
-            g1_tap0=0;
-            g1_tap1=7;
-            break;
-
-            case 8:
-            g1_tap0=1;
-            g1_tap1=8;
-            break;
-
-            case 9:
-            g1_tap0=2;
-            g1_tap1=9;
-            break;
-
-            case 10:
-            g1_tap0=1;
-            g1_tap1=2;
-            break;
-
-            case 11:
-            g1_tap0=2;
-            g1_tap1=3;
-            break;
-
-            case 12:
-            g1_tap0=4;
-            g1_tap1=5;
-            break;
-
-            case 13:
-            g1_tap0=5;
-            g1_tap1=6;
-            break;
-
-            case 14:
-            g1_tap0=6;
-            g1_tap1=7;
-            break;
-
-            case 15:
-            g1_tap0=7;
-            g1_tap1=8;
-            break;
-
-            case 16:
-            g1_tap0=8;
-            g1_tap1=9;
-            break;
-
-            case 17:
-            g1_tap0=0;
-            g1_tap1=3;
-            break;
-
-            case 18:
-            g1_tap0=1;
-            g1_tap1=4;
-            break;
-
-            case 19:
-            g1_tap0=2;
-            g1_tap1=5;
-            break;
-
-            case 20:
-            g1_tap0=3;
-            g1_tap1=6;
-            break;
-
-            case 21:
-            g1_tap0=4;
-            g1_tap1=7;
-            break;
-
-            case 22:
-            g1_tap0=5;
-            g1_tap1=8;
-            break;
-
-            case 23:
-            g1_tap0=0;
-            g1_tap1=2;
-            break;
-
-            case 24:
-            g1_tap0=3;
-            g1_tap1=5;
-            break;
-
-            case 25:
-            g1_tap0=4;
-            g1_tap1=6;
-            break;
-
-            case 26:
-            g1_tap0=5;
-            g1_tap1=7;
-            break;
-
-            case 27:
-            g1_tap0=6;
-            g1_tap1=8;
-            break;
-
-            case 28:
-            g1_tap0=7;
-            g1_tap1=9;
-            break;
-
-            case 29:
-            g1_tap0=0;
-            g1_tap1=5;
-            break;
-
-            case 30:
-            g1_tap0=1;
-            g1_tap1=6;
-            break;
-
-            case 31:
-            g1_tap0=2;
-            g1_tap1=7;
-            break;
-
-            case 32:
-            g1_tap0=3;
-            g1_tap1=8;
-            break;
-
-            default: // default
-            code_selection=1;
-            g1_tap0=1;
-            g1_tap1=5;
+          if(j == 0)
+          {
+            g0[j] = g0_state0_next;
+            g1[j] = g1_state0_next;
+          }
+          else
+          {
+            g0[j] = g0[j-1];
+            g1[j] = g1[j-1];
+          }
         }
-
-        // calculate our code LUT
-
-        char g0[10] = {1,1,1,1,1,1,1,1,1,1};
-        char g1[10] = {1,1,1,1,1,1,1,1,1,1};
-        char g1_out = 0;
-
-        int i = 0, j = 0;
-        for(i = 0 ; i < 1023 ; ++i)
-        {
-            g1_out = g1[g1_tap0] ^ g1[g1_tap1];
-
-            code_LUT[i] = (gr_complex) (2.0 * ((g1_out ^ g0[9]) - 0.5));
-
-            char g0_state0_next = g0[2] ^ g0[9];
-            char g1_state0_next = g1[1] ^ g1[2] ^ g1[5]
-                                 ^ g1[7] ^ g1[8] ^ g1[9];
-
-            for(j = 9; j >= 0; --j)
-            {
-                if(j == 0)
-                {
-                    g0[j] = g0_state0_next;
-                    g1[j] = g1_state0_next;
-                }
-                else
-                {
-                    g0[j] = g0[j-1];
-                    g1[j] = g1[j-1];
-                }
-            }
-        }
+      }
     }
 
     void gps_codegen_c_impl::set_sample_rate(float samplerate)
     {
-        // fixed point with 40 bits fractional part
-        code_phase_increment = (unsigned long)(( 1023e3 / samplerate) * (float)(1L << 40));  
-
+      // fixed point with 40 bits fractional part
+      code_phase_increment = (unsigned long)(( 1023e3 / samplerate) * (float)(1L << 40));  
     }
 
     void
     gps_codegen_c_impl::set_datamode(int mode)
     {
-        datagen_mode = mode;
+      datagen_mode = mode;
     }
 
     void
     gps_codegen_c_impl::advance_random_data()
     {
-        // 22-bit m-sequence LFSR
-        char new_bit = (random_data[21] ^ random_data[20]);
+      // 22-bit m-sequence LFSR
+      char new_bit = (random_data[21] ^ random_data[20]);
 
-        for(int i = 21; i >= 0; --i)
+      for(int i = 21; i >= 0; --i)
+      {
+        if(i == 0)
         {
-            if(i == 0)
-            {
-                random_data[i] = new_bit & 1;
-            }
-            else
-            {
-                random_data[i] = random_data[i-1];
-            }
+          random_data[i] = new_bit & 1;
         }
+        else
+        {
+          random_data[i] = random_data[i-1];
+        }
+      }
     }
 
     int
@@ -313,44 +312,44 @@ namespace gr {
               gr_vector_const_void_star &input_items,
               gr_vector_void_star &output_items)
     {
-        gr_complex *out = (gr_complex *) output_items[0];
+      gr_complex *out = (gr_complex *) output_items[0];
 
-        // Do <the thing>
-        int i;
-        int code_sel = 0;
+      // Do <the thing>
+      int i;
+      int code_sel = 0;
 
-        for(i = 0; i < noutput_items; ++i)
+      for(i = 0; i < noutput_items; ++i)
+      {
+        code_sel = (int) ((code_phase + (1L << 39)) >> 40 );
+
+        if(datagen_mode == 1)
+          out[i] = ( (gr_complex)(((float)random_data[21] - 0.5f) * 2.0f) ) * code_LUT[code_sel];
+        else
+          out[i] = code_LUT[code_sel];
+
+        code_phase += code_phase_increment;
+
+        long limit = (1023L << 40);
+
+        if( code_phase >=  limit)
         {
-            code_sel = (int) ((code_phase + (1L << 39)) >> 40 );
-
-            if(datagen_mode == 1)
-                out[i] = ( (gr_complex)(((float)random_data[21] - 0.5f) * 2.0f) ) * code_LUT[code_sel];
-            else
-                out[i] = code_LUT[code_sel];
-
-            code_phase += code_phase_increment;
-
-            long limit = (1023L << 40);
-
-            if( code_phase >=  limit)
+          if(datagen_mode == 1)
+          {
+            if(data_counter == 20)
             {
-                if(datagen_mode == 1)
-                {
-                    if(data_counter == 20)
-                    {
-                        data_counter = 1;
-                        advance_random_data();
-                    }
-                    else
-                        data_counter++;
-                }
-
-                code_phase -= (1023L << 40);
+              data_counter = 1;
+              advance_random_data();
             }
-        }
+            else
+              data_counter++;
+          }
 
-        // Tell runtime system how many output items we produced.
-        return noutput_items;
+          code_phase -= (1023L << 40);
+        }
+      }
+
+      // Tell runtime system how many output items we produced.
+      return noutput_items;
     }
 
   } /* namespace gps */
--- lib/gps_navdata_impl.cc
+++ lib/gps_navdata_impl.cc
@@ -43,13 +43,13 @@ namespace gr {
               gr::io_signature::make(1, 1, sizeof(char)),
               gr::io_signature::make(1, 1, sizeof(char)))
     {
-        fsm = search_preamble;
+      fsm = search_preamble;
 
-        for(int i = 0; i < 30 ; ++i)
-            word[i] = 0;
+      for(int i = 0; i < 30 ; ++i)
+        word[i] = 0;
 
-        last2[0] = 0;
-        last2[1] = 0;
+      last2[0] = 0;
+      last2[1] = 0;
     }
 
     /*
@@ -62,72 +62,71 @@ namespace gr {
     void
     gps_navdata_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
     {
- 
-        ninput_items_required[0] = noutput_items;
+         ninput_items_required[0] = noutput_items;
     }
 
     int
     gps_navdata_impl::check_preamble(unsigned char *d, unsigned char &p)
     {
-        unsigned char preamble[8] = {1, 0, 0, 0, 1, 0, 1, 1};
+      unsigned char preamble[8] = {1, 0, 0, 0, 1, 0, 1, 1};
 
+      p = 0;
+
+      int ok = 1;
+      for(int i = 0; i < 8 ; ++i)
+      {
+        if(d[i] != preamble[i])
+          ok = 0;
+      }
+
+      if(ok)
+      {
         p = 0;
+        return 1;
+      }
 
-        int ok = 1;
-        for(int i = 0; i < 8 ; ++i)
-        {
-            if(d[i] != preamble[i])
-                ok = 0;
-        }
+      ok = 1;
+      for(int i = 0; i < 8 ; ++i)
+      {
+        if(d[i] != (preamble[i] ^ 1))
+          ok = 0;
+      }
 
-        if(ok)
-        {
-            p = 0;
-            return 1;
-        }
+      if(ok)
+      {
+        p = 1;
+        return 1;
+      }
 
-        ok = 1;
-        for(int i = 0; i < 8 ; ++i)
-        {
-            if(d[i] != (preamble[i] ^ 1))
-                ok = 0;
-        }
-
-        if(ok)
-        {
-            p = 1;
-            return 1;
-        }
-
-        return 0;
+      return 0;
     }
 
     int
     gps_navdata_impl::check_checksum(unsigned char *d, unsigned char *D2)
     {
-        unsigned char D[6];
+      unsigned char D[6];
 
-        //for(int i = 0; i < 24 ; ++i)
-        //{
-        //    D[i] = d[i] ^ d[29];
-        //}
+      //for(int i = 0; i < 24 ; ++i)
+      //{
+      //    D[i] = d[i] ^ d[29];
+      //}
 
-        D[0] = D2[0] ^ d[0] ^ d[1] ^ d[2] ^ d[4] ^ d[5] ^ d[ 9] ^ d[10] ^ d[11] ^ d[12] ^ d[13] ^ d[16] ^ d[17] ^ d[19] ^ d[22];
-        D[1] = D2[1] ^ d[1] ^ d[2] ^ d[3] ^ d[5] ^ d[6] ^ d[10] ^ d[11] ^ d[12] ^ d[13] ^ d[14] ^ d[17] ^ d[18] ^ d[20] ^ d[23];
-        D[2] = D2[0] ^ d[0] ^ d[2] ^ d[3] ^ d[4] ^ d[6] ^ d[ 7] ^ d[11] ^ d[12] ^ d[13] ^ d[14] ^ d[15] ^ d[18] ^ d[19] ^ d[21];
-        D[3] = D2[1] ^ d[1] ^ d[3] ^ d[4] ^ d[5] ^ d[7] ^ d[ 8] ^ d[12] ^ d[13] ^ d[14] ^ d[15] ^ d[16] ^ d[19] ^ d[20] ^ d[22];
-        D[4] = D2[1] ^ d[0] ^ d[2] ^ d[4] ^ d[5] ^ d[6] ^ d[ 8] ^ d[ 9] ^ d[13] ^ d[14] ^ d[15] ^ d[16] ^ d[17] ^ d[20] ^ d[21] ^ d[23];
-        D[5] = D2[1] ^ d[2] ^ d[4] ^ d[5] ^ d[7] ^ d[8] ^ d[ 9] ^ d[10] ^ d[12] ^ d[14] ^ d[18] ^ d[21] ^ d[22] ^ d[23];
+      D[0] = D2[0] ^ d[0] ^ d[1] ^ d[2] ^ d[4] ^ d[5] ^ d[ 9] ^ d[10] ^ d[11] ^ d[12] ^ d[13] ^ d[16] ^ d[17] ^ d[19] ^ d[22];
+      D[1] = D2[1] ^ d[1] ^ d[2] ^ d[3] ^ d[5] ^ d[6] ^ d[10] ^ d[11] ^ d[12] ^ d[13] ^ d[14] ^ d[17] ^ d[18] ^ d[20] ^ d[23];
+      D[2] = D2[0] ^ d[0] ^ d[2] ^ d[3] ^ d[4] ^ d[6] ^ d[ 7] ^ d[11] ^ d[12] ^ d[13] ^ d[14] ^ d[15] ^ d[18] ^ d[19] ^ d[21];
+      D[3] = D2[1] ^ d[1] ^ d[3] ^ d[4] ^ d[5] ^ d[7] ^ d[ 8] ^ d[12] ^ d[13] ^ d[14] ^ d[15] ^ d[16] ^ d[19] ^ d[20] ^ d[22];
+      D[4] = D2[1] ^ d[0] ^ d[2] ^ d[4] ^ d[5] ^ d[6] ^ d[ 8] ^ d[ 9] ^ d[13] ^ d[14] ^ d[15] ^ d[16] ^ d[17] ^ d[20] ^ d[21] ^ d[23];
+      D[5] = D2[1] ^ d[2] ^ d[4] ^ d[5] ^ d[7] ^ d[8] ^ d[ 9] ^ d[10] ^ d[12] ^ d[14] ^ d[18] ^ d[21] ^ d[22] ^ d[23];
 
-        int ok = 1;
+      int ok = 1;
 
-        for(int i = 0; i < 6; ++i)
-        {
-            if(D[i] != d[i + 24])
-                ok = 0;
-        }
+      for(int i = 0; i < 6; ++i)
+      {
+        if(D[i] != d[i + 24])
+          ok = 0;
+      }
 
-        return ok;
+      return ok;
     }
 
     int
@@ -136,150 +135,146 @@ namespace gr {
                        gr_vector_const_void_star &input_items,
                        gr_vector_void_star &output_items)
     {
-        const char *in = (const char *) input_items[0];
-        char*out = (char *) output_items[0];
+      const char *in = (const char *) input_items[0];
+      char*out = (char *) output_items[0];
 
+      // Do <+signal processing+>
+      // Tell runtime system how many input items we consumed on
+      // each input stream.
+      for(int i = 0; i < noutput_items;++i)
+      {
 
-
-        // Do <+signal processing+>
-        // Tell runtime system how many input items we consumed on
-        // each input stream.
-        for(int i = 0; i < noutput_items;++i)
+        switch(fsm)
         {
+            case search_preamble:
+              last2[0] = last2[1];
+              last2[1] = word[0];
+
+              for(int j = 0; j < 7 ; ++j)
+              word[j] = word[j+1];
+
+              word[7] = in[i];
+
+              if(check_preamble(word, polarity))
+              {
+                // preamble found
+
+                printf("Preamble found. \n");
+
+                bit_counter = 8;
+                fsm = read_tlm_word;
+              }
+
+              break;
+
+            case read_tlm_word:
+
+              word[bit_counter++] = in[i] ^ polarity;
+
+              if(bit_counter == 30)
+              {
+                if(!check_preamble(word, polarity))
+                {
+                  printf("TLM word has wrong preamble.\n");
 
 
-            switch(fsm)
-            {
+                  fsm = search_preamble;
 
-                case search_preamble:
-                    last2[0] = last2[1];
-                    last2[1] = word[0];
+                  last2[1] = word[29];
+                  last2[0] = word[28];
+                } 
+                else if(!check_checksum(word, last2))
+                {
+                  printf("TLM word checksum failed.\n");
 
-                    for(int j = 0; j < 7 ; ++j)
-                        word[j] = word[j+1];
+                  last2[1] = word[29];
+                  last2[0] = word[28];
+                  fsm = search_preamble;
+                }
+                else
+                {
+                  printf("TLM word checksum OK!\n");
 
-                    word[7] = in[i];
+                  last2[1] = word[29];
+                  last2[0] = word[28];
+                  fsm = read_how_word;
+                }
 
-                    if(check_preamble(word, polarity))
-                    {
-                        // preamble found
+                bit_counter = 0;
+              }
 
-                        printf("Preamble found. \n");
+              break;
 
-                        bit_counter = 8;
-                        fsm = read_tlm_word;
-                    }
+            case read_how_word:
+              word[bit_counter++] = in[i] ^ polarity;
 
-                    break;
+              if(bit_counter == 30)
+              {
+                if(!check_checksum(word, last2))
+                {
+                  printf("HOW word checksum failed.\n");
 
-                case read_tlm_word:
+                  last2[1] = word[29];
+                  last2[0] = word[28];
+                  //fsm = search_preamble;
+                }
+                else
+                {
+                  printf("HOW word checksum OK!\n");
 
-                    word[bit_counter++] = in[i] ^ polarity;
+                  last2[1] = word[29];
+                  last2[0] = word[28];
+                  //fsm = read_subframe;
+                }
 
-                    if(bit_counter == 30)
-                    {
-                        if(!check_preamble(word, polarity))
-                        {    
-                            printf("TLM word has wrong preamble.\n");
+                fsm = read_subframe;
+                bit_counter = 0;
+                word_counter = 2;
+              }
+              break;
 
+            case read_subframe:
+              word[bit_counter++] = in[i] ^ polarity; 
 
-                            fsm = search_preamble;
+              if(bit_counter == 30)
+              {
+                if(!check_checksum(word, last2))
+                {
+                  printf("Word %d checksum failed.\n", word_counter);
 
-                            last2[1] = word[29];
-                            last2[0] = word[28];
-                        } 
-                        else if(!check_checksum(word, last2))
-                        {
-                            printf("TLM word checksum failed.\n");
+                  last2[1] = word[29];
+                  last2[0] = word[28];
+                  //fsm = search_preamble;
+                }
+                else
+                {
+                  printf("Word %d checksum OK!\n", word_counter);
 
-                            last2[1] = word[29];
-                            last2[0] = word[28];
-                            fsm = search_preamble;
-                        }                        
-                        else
-                        {
-                            printf("TLM word checksum OK!\n");
+                  last2[1] = word[29];
+                  last2[0] = word[28];
+                  //fsm = read_how_word;
+                }
 
-                            last2[1] = word[29];
-                            last2[0] = word[28];
-                            fsm = read_how_word;
-                        }
+                if(++word_counter == 10)
+                {
+                  word_counter = 0;
+                  fsm = read_tlm_word;
+                } 
 
-                        bit_counter = 0;
-                    }
+                bit_counter = 0;
+              }
+              break;
 
-                    break;
-
-                case read_how_word:
-                    word[bit_counter++] = in[i] ^ polarity;
-
-                    if(bit_counter == 30)
-                    {
-                        if(!check_checksum(word, last2))
-                        {
-                            printf("HOW word checksum failed.\n");
-
-                            last2[1] = word[29];
-                            last2[0] = word[28];
-                            //fsm = search_preamble;
-                        }
-                        else
-                        {
-                            printf("HOW word checksum OK!\n");
-
-                            last2[1] = word[29];
-                            last2[0] = word[28];
-                            //fsm = read_subframe;
-                        }
-
-                        fsm = read_subframe;
-                        bit_counter = 0;
-                        word_counter = 2;
-                    }
-                    break;
-
-                case read_subframe:
-                    word[bit_counter++] = in[i] ^ polarity; 
-
-                    if(bit_counter == 30)
-                    {
-                        if(!check_checksum(word, last2))
-                        {
-                            printf("Word %d checksum failed.\n", word_counter);
-
-                            last2[1] = word[29];
-                            last2[0] = word[28];
-                            //fsm = search_preamble;
-                        }
-                        else
-                        {
-                            printf("Word %d checksum OK!\n", word_counter);
-
-                            last2[1] = word[29];
-                            last2[0] = word[28];
-                            //fsm = read_how_word;
-                        }
-
-                        if(++word_counter == 10)
-                        {
-                            word_counter = 0;
-                            fsm = read_tlm_word;
-                        } 
-
-                        bit_counter = 0;
-                    }
-                    break;
-
-                default:
-                    fsm = search_preamble;
-            }
+            default:
+              fsm = search_preamble;
         }
+      }
 
-        fflush(stdout);
-        consume_each (noutput_items);
+      fflush(stdout);
+      consume_each (noutput_items);
 
-        // Tell runtime system how many output items we produced.
-        return noutput_items;
+      // Tell runtime system how many output items we produced.
+      return noutput_items;
     }
 
   } /* namespace gps */
